22271b5d1ea4e1ec007836e19e4df2249699a199
Noland Seigler <nolandseigler@gmail.com>
2020-03-16 11:13:43 -0500

    reviewing in class

---
+++ src/main/java/com/europa/springblog/models/Post.java
@@ -0,0 +1,36 @@
+package com.europa.springblog.models;
+
+public class Post {
+    private String title;
+    private String body;
+    private int id;
+
+    public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
