66a9fa253448f4e2036bf3413807c5f9ea41eb06
Noland Seigler <nolandseigler@gmail.com>
2020-03-16 10:27:51 -0500

    added some formatting

--- src/main/java/com/europa/springblog/controllers/RollDiceController.java
+++ src/main/java/com/europa/springblog/controllers/RollDiceController.java
@@ -35,18 +35,19 @@ class RollDiceController {
         String outputString = "";
         int rollsNumber = getRandomNumberInRange(2, 5);
         List<String> resultList = new ArrayList<>();
-        for(int i = 0; i <= rollsNumber; i++) {
-            if(i == 0) {
-                resultList.add(String.format("You rolled %d dice.", rollsNumber));
-            }
+        resultList.add(String.format("You rolled %d dice.", rollsNumber));
+        int numCorrect = 0;
+        for(int i = 1; i <= rollsNumber; i++) {
             int dieNumber = getRandomNumberInRange(1, 6);
             if (userNumber == dieNumber) {
+                numCorrect++;
                 outputString = String.format("You guessed the correct number! The die landed on %d.", dieNumber);
             } else {
                 outputString = String.format("You guessed the incorrect number! The die landed on %d and your number was %d.", dieNumber, userNumber);
             }
             resultList.add(outputString);
         }
+        resultList.add(String.format("You guessed correctly %d times.", numCorrect));
         model.addAttribute("results", resultList);
         return "dice-rolled";
     }
