0dfecaec305ec578fd2c29f64aa509cea15bebb7
Noland Seigler <nolandseigler@gmail.com>
2020-03-20 13:46:47 -0500

    added justin's seeder. have not tested

---
+++ seeder.sql
@@ -0,0 +1,17 @@
+use blog_db;
+
+
+# Seed posts
+
+insert into posts (title, body) values ('Test Post Title 1', 'This is the body for test post 1');
+insert into posts (title, body) values ('Test Post Title 2', 'This is the body for test post 2');
+insert into posts (title, body) values ('Test Post Title 3', 'This is the body for test post 3');
+insert into posts (title, body) values ('Test Post Title 4', 'This is the body for test post 4');
+insert into posts (title, body) values ('Test Post Title 5', 'This is the body for test post 5');
+
+
+# Seed ads
+
+insert into ads (title, description) values ('intermediate', 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est. Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum. Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy.');
+insert into ads (title, description) values ('Self-enabling', 'Donec posuere metus vitae ipsum. Aliquam non mauris. Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis. Fusce posuere felis sed lacus.');
+insert into ads (title, description) values ('Graphic Interface', 'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.');
