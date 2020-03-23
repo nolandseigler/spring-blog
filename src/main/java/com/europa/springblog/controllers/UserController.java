a37ab51e3f629261c12b473add2ecee6f07186d0
Noland Seigler <nolandseigler@gmail.com>
2020-03-23 12:27:49 -0500

    checkpoint

--- src/main/java/com/europa/springblog/controllers/UserController.java
+++ src/main/java/com/europa/springblog/controllers/UserController.java
@@ -1,4 +1,35 @@
 package com.europa.springblog.controllers;
 
+import com.europa.springblog.models.User;
+import com.europa.springblog.repositories.UserRepo;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class UserController {
+    private UserRepo userDao;
+    private PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepo userDao, PasswordEncoder passwordEncoder) {
+        this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/sign-up")
+    public String showSignupForm(Model model){
+        model.addAttribute("user", new User());
+        return "users/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        userDao.save(user);
+        return "redirect:/login";
+    }
 }
--- src/main/java/com/europa/springblog/services/UserDetailsLoader.java
+++ src/main/java/com/europa/springblog/services/UserDetailsLoader.java
@@ -1,4 +1,28 @@
 package com.europa.springblog.services;
 
-public class UserDetailsLoader {
+import com.europa.springblog.models.User;
+import com.europa.springblog.models.UserWithRoles;
+import com.europa.springblog.repositories.UserRepo;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsLoader implements UserDetailsService {
+    private final UserRepo userDao;
+
+    public UserDetailsLoader(UserRepo userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userDao.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("No user found for " + username);
+        }
+
+        return new UserWithRoles(user);
+    }
 }
